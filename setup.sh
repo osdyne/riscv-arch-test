63d3195d56fb3de1dcd523d48353f86999922ac0
Daniel Lehmann <daniel@osdyne.com>
2026-03-13 02:00:24 +0000

    Adjust setup.sh instructions for rv32 build

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-# One time setup needed to build and run tests for rv64 (rv32 would also require a custom toolchain)
+# One time setup needed to build and run tests for rv64 and rv32
 
-# Tested against Ubuntu 24.04 in Feb 2025. Spike and the other tools change occasionally, breaking things below
+# Tested against Ubuntu 24.04 in Mar 2025. Spike and the other tools change occasionally, breaking things below
 
-sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev python3.6 gcc-riscv64-unknown-elf device-tree-compiler
+sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev python3.6 gcc-riscv64-unknown-elf device-tree-compiler ninja-build
 
 python3 -m venv env/venv
 
@@ -22,3 +22,12 @@ cd build
 ../configure --prefix $(pwd)/../../spike
 make -j16
 cd ../../..
+
+# Get rv32 toolchain
+cd env
+mkdir rv32-target
+git clone https://github.com/riscv-collab/riscv-gnu-toolchain
+cd riscv-gnu-toolchain
+./configure --prefix=$(pwd)/../rv32-target --with-arch=rv32gc --with-abi=ilp32d
+make -j16
+cd ../..
